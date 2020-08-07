dc92d60669c85a457312acaa17c79953701164eb
Giovanni Tjahjono <ivangiovn@gmail.com>
2020-08-07 14:23:55 +0800

    Adding front-end

--- back-end/model/posting-model.js
+++ back-end/model/posting-model.js
@@ -5,9 +5,14 @@ const mongoose = require('mongoose');
 // });
 const postingSchema = mongoose.Schema({
     title_posting: { type: String, required: true },
-    owner_posting: { type: String, required: true },
+    image_posting: { type: String, required: true },
+    quote_posting: { type: String, required: true },
+    year_of_posting: { type: Date, required: true },
+    owner_id_posting: { type: String, required: true },
+    owner_name_posting: { type: String, required: true },
     description_posting: { type: String, required: true },
-    total_view_posting: { type: Number, required: true }
+    skills: { type: String, required: true }
 });
 //// export untuk model 
 module.exports = mongoose.model('posting', postingSchema);
+
