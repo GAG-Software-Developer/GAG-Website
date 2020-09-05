cea8c44a90d9aaf0b789bf94e99ffeea20a6663f
Giovanni Tjahjono <ivangiovn@gmail.com>
2020-09-05 14:31:43 +0800

    Adding about front-end

--- front-end/src/views/components/essential/Navigasi.js
+++ front-end/src/views/components/essential/Navigasi.js
@@ -13,7 +13,7 @@ export default function Navigasi(props) {
         <Navbar className="justify-content-around shadow p-3 mb-5 bg-white rounded" sticky="top" style={nav_colour}>
             <Nav style={{ fontWeight: "bold" }} >
                 { props.selectedPage == "explore" ? ( <Link className="nav-link" style={text_selected} to="/">Explore</Link>) :  <Link className="nav-link" to="/">Explore</Link> }
-                { props.selectedPage == "technology" ? ( <Link className="nav-link" style={text_selected} to="/technology">Technology</Link>) :  <Link className="nav-link" to="/technology">Technology</Link> }
+                { props.selectedPage == "technology" ? ( <Link className="nav-link" style={text_selected} to="/technology">Code Labs</Link>) :  <Link className="nav-link" to="/technology">Laboratory</Link> }
                 { props.selectedPage == "tutorial" ? ( <Link className="nav-link" style={text_selected} to="/tutorial">Tutorial</Link>) :  <Link className="nav-link" to="/tutorial">Tutorial</Link> }
                 { props.selectedPage == "about" ? ( <Link className="nav-link" style={text_selected} to="/about">About</Link>) :  <Link className="nav-link" to="/about">About</Link> }
             </Nav>
